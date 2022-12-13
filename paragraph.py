ea80eb9f837e6c727a903dba82bf3870c4e8e0ee
Anil-Gehlot <anilgehlot2000@gmail.com>
2022-12-13 23:26:19 +0530

    removed

--- paragraph.py
+++
@@ -1,4 +0,0 @@
-paragraph = """The Baahubali actor Prabhas is one of the highest paid actors in the country. The actor who gained worldwide 
-               recognition after featuring in S.S. Rajamouli’s Baahubali, the actor prefers to keep his private life away from 
-                 the prying eyes of media.However, his lavish farmhouse located in the Jubilee Hills of Hyderabad caught the attention."""
-
