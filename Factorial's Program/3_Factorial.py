4f250d11aa11f97751cc69349b75365f2ec196c8
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-01-05 09:53:38 +0530

    .

--- Factorial's Program/3_Factorial.py
+++
@@ -1,9 +0,0 @@
-
-num=int(input("enter a no. : "))
-fact=1
-if num==0 or num==1:
-	print("{} is factorial of {}".format(fact,num))
-else:
-	for j in range(1,num+1):
-		fact=fact*j
-	print("{} is factorial of {}".format(fact,num))
