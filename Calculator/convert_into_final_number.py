481cc83cc3a50a7e6c3c832029f43e9303c711f4
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-01-06 22:11:26 +0530

    Dojo Activity

---
+++ Calculator/convert_into_final_number.py
@@ -0,0 +1,10 @@
+def convert(p):
+    temp1=int(p)
+    temp2=p-temp1
+
+    if temp2 <=0:
+        final=int(p)
+    else:
+        final=float(p)
+    print("\nThe final answer is : ",final)
+    return final
