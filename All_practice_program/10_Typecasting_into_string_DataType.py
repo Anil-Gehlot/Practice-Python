331dcb307203fba4564bc4f6f01526039622e253
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-02-24 11:00:02 +0530

    Typecasting into string DataType

---
+++ All_practice_program/10_Typecasting_into_string_DataType.py
@@ -0,0 +1,43 @@
+"""
+Typecasting :- converting one datatype into another datatype is known as Typecasting or 
+                Typeconversion or Type coersion.
+                There are 5 built-in function in python for typecasting-->>
+                1. int()
+                2. float()
+                3. complex()
+                4. bool()
+                5. str()
+                """
+
+A = 234            # integer value
+a = 123.456        # float value
+b = 10+20j         # complex value
+c = True           # boolean value
+d = False          # boolean value
+e = "50"           # integer value as a string
+f = "10.2"         # float value as a atring  
+g = "anil"         #  string value
+h = 0B11110        # binary value
+i = 0O11567        # octal value
+j = 0X1bee01       # hexa-decimal value
+k = "0B11110"        # binary value in string form 
+l = "0O11567"        # octal value in string form
+m = "0X1bee01"       # hexa-decimal value in string form
+
+# typecasting all datatype into string type :-
+# each and every data type can easily be converted into string DataType.
+
+print(str(A))     
+print(str(a))
+print(str(b))
+print(str(c))
+print(str(d))
+print(str(e))
+print(str(f))
+print(str(g))
+print(str(h))
+print(str(i))
+print(str(j))
+print(str(k))
+print(str(l))
+print(str(m))
