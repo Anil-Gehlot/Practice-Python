4a7ba8cfe25446549b3569aaa7e51de064f2652d
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-03-04 17:03:22 +0530

    seperator.join(group of strings)

---
+++ All_practice_program/29_All_about_string/12_Joining_of_Strings.py
@@ -0,0 +1,11 @@
+'''
+We can join a group of strings(list or tuple) with respect to the given seperator.
+a = seperator.join(group of strings)
+'''
+
+list = ['hello',"I'm","ANIL",'gehlot']
+tup=('hyderabad','singapore','london','dubai')
+
+print('-'.join(list))       # hello-I'm-ANIL-gehlot
+print(":".join(tup))        # hyderabad:singapore:london:dubai
+
