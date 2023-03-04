e2589f62f34a18f0a6731a7ca228fbed0e0ec950
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-03-04 17:29:38 +0530

    sorted()

---
+++ All_practice_program/29_All_about_string/15.5_To_sort_string_according_to_ascii_value.py
@@ -0,0 +1,12 @@
+'''
+sorted() method is used to sort the given string according to their ascii value.
+'''
+
+str1 = 'ABCDasdfdh'
+str2 = 'absdf98r302'
+str3 = 'djhf89SJC'
+ 
+print(sorted(str1))     # ['A', 'B', 'C', 'D', 'a', 'd', 'd', 'f', 'h', 's']
+print(sorted(str2))     # ['0', '2', '3', '8', '9', 'a', 'b', 'd', 'f', 'r', 's']
+print(sorted(str3))     # ['8', '9', 'C', 'J', 'S', 'd', 'f', 'h', 'j']
+
