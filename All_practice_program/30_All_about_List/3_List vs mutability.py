305480fdbd0dc485a05624991734aebd202ef4d8
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-03-06 15:47:37 +0530

    list is mutable

---
+++ All_practice_program/30_All_about_List/3_List vs mutability.py
@@ -0,0 +1,9 @@
+'''
+Once we creates a List object,we can modify its content. 
+Hence List objects are mutable.
+'''
+
+list1 = [10,20,30,40]
+
+list1[3]=999        # [10, 20, 30, 999]
+print(list1)
