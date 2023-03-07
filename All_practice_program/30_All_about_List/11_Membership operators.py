bcc113e9397beb446be0440a48ef9cd1026ae156
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-03-07 14:31:15 +0530

    in & not in

---
+++ All_practice_program/30_All_about_List/11_Membership operators.py
@@ -0,0 +1,12 @@
+'''
+We can check whether element is a member of the list or not by using memebership operators.
+    Membership operator : 1) in  & 2) not in
+'''
+
+n = [10,20,30,40]
+
+print(10 in n)          # True
+print(10 not in n)      # False
+
+print(50 in n)          # False
+print(50 not in n)      # True
