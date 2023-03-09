e8f89cceb6c8ce7575a95c1355dc5e0cfa775ae1
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-03-09 12:57:30 +0530

    comperehnsion is applicable on dictionary

---
+++ All_practice_program/33_All_about_dictionary/6_Dictionary Comprehension.py
@@ -0,0 +1,8 @@
+
+# Comprehension concept applicable for dictionaries also.
+
+square = {x:x*x for x in range(7)}
+print(square)           # {0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36}
+
+double = {x:2*x for x in range(7)}
+print(double)           # {0: 0, 1: 2, 2: 4, 3: 6, 4: 8, 5: 10, 6: 12}
