6c61df2522142b7b505ad03d1669dec6fd03bc11
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-03-09 10:02:49 +0530

    in & not in

---
+++ All_practice_program/32_All_about_set/4_Membership operators.py
@@ -0,0 +1,9 @@
+
+s1 = {'anil', '5',23,24,235,346}
+s2 = set('Prabhas') 
+
+print(s2)               # {'r', 'h', 'P', 'b', 'a', 's'}
+print('p' in s2)        # False
+print("p" in s2)        # False
+print(5 not in s1)      # True
+print("5" not in s1)    # False
