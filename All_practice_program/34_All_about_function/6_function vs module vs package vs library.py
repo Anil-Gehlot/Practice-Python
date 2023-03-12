83ac2408578d1c975eeb7451cf720b213127bb51
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-03-12 10:33:04 +0530

    key-difference among all

---
+++ All_practice_program/34_All_about_function/6_function vs module vs package vs library.py
@@ -0,0 +1,14 @@
+'''
+
+Note : Function vs Module vs Library vs Package
+
+
+1) Function :  A group of lines with some name is called a function.
+
+2) Module : A group of functions saved to a file, is called a Module.
+
+3) Package : A group of modules is known as package.
+
+4) Library : A group of packages is nothing but a library.
+
+'''
