8f529c49e7ec84c2759b82d92f84c79ec7e860ec
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-03-22 10:02:55 +0530

    importing module and using module's variables & functions

---
+++ All_practice_program/35_All_about_Module/2_importing_module.py
@@ -0,0 +1,11 @@
+
+import module
+
+print(module.x)             # 888
+print(module.y)             # 999
+
+module.add(23,56)           # The addition is :  79
+module.sub(353,324)         # The substraction is :  29
+module.product(34,2)        # The multiplication is :  68
+module.div(234,32)          # Divison of both number is :  7.3125
+module.div(2342,0)
