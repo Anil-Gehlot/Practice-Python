c4542b347ce376976e9a54fcf26730e749ce6d49
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-05-27 11:37:00 +0530

    Two Types of Exception

---
+++ All_practice_program/37_Exception Handling/11_Types of exceptions.py
@@ -0,0 +1,26 @@
+'''
+
+In Python there are 2 types of exceptions are possible : 
+
+1. Predefined Exceptions / In-built exceptions
+2. User Definded Exceptions / Customized Exceptions or Programatic Exceptions
+
+
+1. Predefined Exceptions:
+                The exceptions which are raised automatically by Python virtual machine whenver a
+particular event occurs, are called pre defined exceptions.
+
+Example:
+        print(10/0)    ZeroDivisionError.
+        x=int("ten")    ValueError
+
+        
+2. User Defined Exceptions:
+                Some time we have to define and raise exceptions explicitly to indicate that something
+goes wrong ,such type of exceptions are called User Defined Exceptions or Customized
+Exceptions.
+            Programmer is responsible to define these exceptions and Python not having any idea
+about these. Hence we have to raise explicitly based on our requirement by using "raise"
+keyword.
+
+'''
