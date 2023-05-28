f492407d36e8e754a1f007a90b9a910195b4a5ff
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-05-28 10:18:26 +0530

    difference between exception handling and assertion

---
+++ All_practice_program/38_logging and Debugging/07_Exception_Handling_vs_Assertions.py
@@ -0,0 +1,9 @@
+'''
+Exception Handling:
+
+Exception Handling can be used to handle runtime errors.
+                    
+Assertions:
+
+Assertions concept can be used to alert programmer to resolve development time errors.                 
+'''
