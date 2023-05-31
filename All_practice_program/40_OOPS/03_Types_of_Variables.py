d4487889998a6e287ad9ca95c0a891a4284938aa
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-05-31 17:05:32 +0530

    Instance Variables, Static Variables, Local variables

---
+++ All_practice_program/40_OOPS/03_Types_of_Variables.py
@@ -0,0 +1,14 @@
+'''
+
+
+Types of Variables:
+
+Inside Python class 3 types of variables are allowed: 
+
+1. Instance Variables (non-static variable  (or)  Object Level Variables)
+2. Static Variables (Class Level Variables)
+3. Local variables (Method Level Variables)
+
+
+
+'''
