1a19589a00c440c7172da5d4079872b24825757c
Anil-Gehlot <anilgehlot2000@gmail.com>
2023-06-12 11:31:16 +0530

    A class having only abstract methods

---
+++ All_practice_program/43_OOPS part-4/03_interface.py
@@ -0,0 +1,30 @@
+'''
+
+In general, If an abstract class contains only abstract methods, Such type of 
+abstract class is considered as INTERFACE. 
+'''
+
+from abc import *
+class Test():
+    
+    @abstractmethod
+    def m1():
+        pass
+    
+    @abstractmethod
+    def m2():
+        pass
+    
+    @abstractmethod
+    def m3():
+        pass
+    
+    @abstractmethod
+    def m4():
+        pass
+    
+    @abstractmethod
+    def m5():
+        pass
+
+    
