790629eb76f085b8148efab4903789c9da64b631
Anil Gehlot <119098076+Anil-Gehlot@users.noreply.github.com>
2023-08-10 17:37:28 +0530

    finding area of rectangle

---
+++ Function's_program/01_area_of_rectangle.py
@@ -0,0 +1,12 @@
+#program to find the area of different different rectangle using function.
+
+def rect_area():
+	Length = int(input("enter length : "))
+	Breadth = int(input("enter breadth : "))
+	Area = Length* Breadth
+	print("area of rectangle : ",Area)
+
+rect_area()
+rect_area()
+rect_area()
+rect_area()
