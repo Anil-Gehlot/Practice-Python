95a9ffcd9f91ade87d752e610d34a3ff37d0c720
Anil Gehlot <119098076+Anil-Gehlot@users.noreply.github.com>
2023-08-10 17:47:11 +0530

    deleting an specific element by remove() method

---
+++ List's_program/06_deletion_in_list_by_remove()_method.py
@@ -0,0 +1,8 @@
+student1= ['anil','mca','ag7@gmail.com',12]
+
+#The remove() method removes the specified element of the list.
+student1.remove("mca")
+print(student1)
+
+student1.remove(12)
+print(student1)
