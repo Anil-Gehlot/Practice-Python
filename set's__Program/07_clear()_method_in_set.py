848c4db5d65be0dc9f3edfee79e49002fcbbedb2
Anil Gehlot <119098076+Anil-Gehlot@users.noreply.github.com>
2023-09-20 22:23:27 +0530

    removing all the element from the set

--- set's__Program/07_clear()_method_in_set.py
+++ set's__Program/07_clear()_method_in_set.py
@@ -1,5 +1,5 @@
 star={ 2,4,6,1,2,11,"yes","no",2.4,255,2+1j}
 
-#clear() method removes all the element from the set
+#clear() method removes all the element from the set and make that set empty
 star.clear()
 print(star)
